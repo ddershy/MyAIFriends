ab3e4fce46f8008fc54206ceafafa5b5a19f97f3
zqy <lumos_7@163.com>
2026-03-02 18:35:19 +0800

    实现个人主页后端、实现个人主页用户信息区

--- frontend/src/views/user/space/SpaceIndex.vue
+++ frontend/src/views/user/space/SpaceIndex.vue
@@ -1,13 +1,90 @@
 <script setup>
-import {useRoute, useRouter} from "vue-router";
 
-const route = useRoute()//取当前url相关信息
-// const router = useRouter()//操作页面前进或后退
+import UserInfoField from "@/views/user/space/components/UserInfoField.vue";
+import {nextTick, onBeforeUnmount, onMounted, ref, useTemplateRef} from "vue";
+import {useRoute} from "vue-router";
+import api from "@/js/http/api.js";
 
+const userProfile = ref(null)
+const characters = ref([])
+const isLoading = ref(false) //判断当前是否有角色在加载
+const hasCharacter = ref(false) //判断是否还有多余角色
+const sentinelRef = useTemplateRef('sentinel-ref')
+const route = useRoute()
+
+function checkSentinelVisible() {  // 判断哨兵是否能被看到
+  if (!sentinelRef.value) return false
+
+  const rect = sentinelRef.value.getBoundingClientRect()
+  return rect.top < window.innerHeight && rect.bottom > 0
+}
+
+
+async function loadMore(){
+  if(isLoading.value || !hasCharacter.value) return //无角色返回
+  isLoading.value = true
+
+  let newCharacters= [] //定义从云端在家的函数
+  try{
+    const res = await api.get('api/create/character/get_list/',{
+      params:{
+        items_count: characters.value.length,
+        user_id: route.params.user_id,
+      }
+    })
+    const data = res.data
+    if(data.result === 'success'){
+      userProfile.value = data.user_profile //与api/create/character/get_list/返回值一致
+      newCharacters = data.characters
+    }
+  }catch (err){
+    console.log(err)
+  }finally { //实现循环加载逻辑
+    isLoading.value = false
+    if(newCharacters.length === 0){//云端无可加载函数
+      hasCharacter.value = false
+    }else{
+      characters.value.push(...newCharacters)//...为展开列表；
+      await nextTick() //等待渲染完，判断哨兵能否被看到
+
+      if(checkSentinelVisible()){
+        await loadMore()
+      }
+    }
+  }
+}
+
+let observer = null
+onMounted(async () => {
+  await loadMore()
+
+  observer = new IntersectionObserver(//监听器 循环加载
+      entries => {
+        entries.forEach(entry => {
+          if(entry.isIntersecting) {
+            loadMore()
+          }
+        })
+      },
+      {root:null,rootMargin:'2px',threshold:0}//root 判断视窗是否交叉，rootMargin 缩小检测视窗范围提前判断,threshold:交叉的大小为某个值的时候触发
+  )
+  observer.observe(sentinelRef.value)
+})
+
+onBeforeUnmount(() => { //组件移除前释放资源
+  observer?.disconnect()  // 解绑监听器
+})
 </script>
 
 <template>
-个人空间：{{ route.params.user_id}} <!--从网址上取参数，{{写参数}}-->
+  <div class="flex flex-col items-center mb-12">
+    <UserInfoField :userProfile="userProfile"/>
+    <div class="grid grid-cols-[repeat(auto-fill,minmax(240px,1fr))] gap-9 mt-12 justify-items-center w-full px-9">
+    </div>
+    <div ref="sentinel-ref" class="h-2 mt-8 w-100 bg-red-200"></div>
+    <div v-if="isLoading" class="text-gray-500 mt-4">加载中...</div>
+    <div v-else-if="!hasCharacter" class="text-gray-500 mt-4">没有更多的角色了</div>
+  </div>
 </template>
 
 <style scoped>
